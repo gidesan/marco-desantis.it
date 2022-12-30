e4c00092b33de74d400464c79e4657277d4e19d5
Giuseppe de Santis <gdesantis@gmail.com>
2022-12-30 17:56:10 +0100

    title rename

--- app/root.tsx
+++ app/root.tsx
@@ -16,7 +16,7 @@ import LegacyMobileNavMenu from "./legacy/LegacyMobileNavMenu";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Marco Ignazio de Santis",
   viewport: "width=device-width,initial-scale=1",
 });
 
